ca2d235186411a6ad1ab700247fb4754a77965eb
Gurman Singh <gurmansingh57801@gmail.com>
2026-06-11 21:25:51 +0530

    Update Pallindrome_and_Reverse.c

--- Pallindrome_and_Reverse.c
+++ Pallindrome_and_Reverse.c
@@ -5,12 +5,15 @@ int main(){
     
     int n ;
     scanf("%d",&n);
+    
     int rev = 0;
+    
     while(n!=0){
         int r = n%10;
         rev = rev *10 + r;
         n = n/10;
     }
+    
     printf("%d",rev);
     
     return 0;
@@ -21,14 +24,19 @@ int main(){
 #include<stdio.h>
 int main(){
     int n ;
+    
     scanf("%d",&n);
+    
     int rev = 0;
     int original = n;
+    
     while(n!=0){
+        
         int r = n%10;
         rev = rev *10 + r;
         n = n/10;
     }
+    
     if(original == rev){
         printf("Pallindrome");
     }
