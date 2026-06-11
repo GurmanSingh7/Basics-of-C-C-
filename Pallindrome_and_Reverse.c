e45ecedc4782a9fbbfff2d6f6704cfcdf674e179
Gurman Singh <gurmansingh57801@gmail.com>
2026-06-11 21:24:40 +0530

    Update Pallindrome_and_Reverse.c

--- Pallindrome_and_Reverse.c
+++ Pallindrome_and_Reverse.c
@@ -1,18 +1,20 @@
 //Reverse of a number 
 
-// #include<stdio.h>
-// int main(){
-//     int n ;
-//     scanf("%d",&n);
-//     int rev = 0;
-//     while(n!=0){
-//         int r = n%10;
-//         rev = rev *10 + r;
-//         n = n/10;
-//     }
-//     printf("%d",rev);
-//     return 0;
-// }
+#include<stdio.h>
+int main(){
+    
+    int n ;
+    scanf("%d",&n);
+    int rev = 0;
+    while(n!=0){
+        int r = n%10;
+        rev = rev *10 + r;
+        n = n/10;
+    }
+    printf("%d",rev);
+    
+    return 0;
+}
 
 //Pallindrome of a number
 #include<stdio.h>
