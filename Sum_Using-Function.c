e27221275e8a4b03bb213ddb48e1452f8e56d4bb
Gurman Singh <gurmansingh57801@gmail.com>
2026-06-11 21:23:54 +0530

    Update Sum_Using-Function.c

--- Sum_Using-Function.c
+++ Sum_Using-Function.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
+
 int sum(int a,int b){
     int c = a+b;
     return c;
 }
+
 int main(){
+    
     int a , b;
     scanf("%d %d",&a,&b);
     sum(a,b);
     printf("%d", sum(a,b));
+    
     return 0;
 }
